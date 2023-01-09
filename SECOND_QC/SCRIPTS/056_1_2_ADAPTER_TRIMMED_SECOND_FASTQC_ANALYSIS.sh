12d639c3370b59f166cc9c34fee69231934f6ba5
Carlos <89907733+biocompt@users.noreply.github.com>
2023-01-09 16:38:01 +0100

    Add files via upload

---
+++ SECOND_QC/SCRIPTS/056_1_2_ADAPTER_TRIMMED_SECOND_FASTQC_ANALYSIS.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH -c 8
+#SBATCH -t 00:10:00
+#SBATCH --job-name=fastqc_RNA
+#SBATCH --mem-per-cpu=16G
+
+module load cesga/2020 fastqc/0.11.9
+fastqc /mnt/lustre/scratch/nlsas/home/otras/fmx/cpm/RNASEQ_CEREBRO_RATAS/TRIMMED/056_1_2_ADAPTER_TRIMMED.fastq.gz --noextract -t 8 -o /mnt/lustre/scratch/nlsas/home/otras/fmx/cpm/RNASEQ_CEREBRO_RATAS/SECOND_FASTQC
