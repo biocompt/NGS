a9388e1e7be93f5850c45a7adb4968a00c133318
Carlos <89907733+biocompt@users.noreply.github.com>
2023-01-09 16:33:07 +0100

    Add files via upload

---
+++ FIRST_QC/SCRIPTS/109_1_1_FASTQC_ANALYSIS.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH -c 8
+#SBATCH -t 00:10:00
+#SBATCH --job-name=fastqc_RNA
+#SBATCH --mem-per-cpu=16G
+
+module load cesga/2020 fastqc/0.11.9
+fastqc /mnt/lustre/scratch/nlsas/home/otras/fmx/cpm/RNASEQ_CEREBRO_RATAS/RAW_FASTQ/109_1_1.fastq.gz --noextract -t 64 -o /mnt/lustre/scratch/nlsas/home/otras/fmx/cpm/RNASEQ_CEREBRO_RATAS/FASTQC
